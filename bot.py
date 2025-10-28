c15b14bac9ce01e6594d5dbafa08c59ecdd666d1
JET-SOUZA <103953894+JET-SOUZA@users.noreply.github.com>
2025-10-28 15:43:35 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -438,3 +438,4 @@ if __name__ == "__main__":
         flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
     except (KeyboardInterrupt, SystemExit):
         print("Bot encerrado.")
+
